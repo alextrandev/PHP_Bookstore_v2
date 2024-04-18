a4d7ccff791c1c124b5d0887dfd80b9c1a68049e
dk10119 <tranduclong092@gmail.com>
2024-04-18 18:04:07 +0300

    import 40 books into db

--- php/components/config.php
+++ php/components/config.php
@@ -1,2 +1,14 @@
 <?php
-define(BASE_URL, "http://localhost:8080/");
+define('BASE_URL', "http://localhost:8080/");
+
+$dbhost = "mysql";
+$dbname = "db";
+$dbuser = "root";
+$dbpass = "nimda";
+
+try {
+    $pdo = new PDO("mysql:host=$dbhost; dbname=$dbname", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection error: ", $e->getMessage();
+}
