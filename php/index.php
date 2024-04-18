01534d326cc671a4af8a1bce63132eaf3e5764fc
dk10119 <tranduclong092@gmail.com>
2024-04-18 20:14:01 +0300

    display books from db

--- php/index.php
+++ php/index.php
@@ -1,14 +1,34 @@
-<?php require_once './components/header.php'; ?>
+<?php require_once './components/header.php';
+
+$stmt = $pdo->prepare("SELECT * FROM books");
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
 
 <h2><?= $_GET["genre"] ?? "All books" ?></h2>
-<section class="book">
-    <a class="bookmark fa fa-star" href="set_favorite.php?add=0" target="_blank"></a>
-    <h3>Book title</h3>
-    <p class="publishing_info">
-        <span class="author">Author</span>,
-        <span class="year">Year</span>
-    </p>
-    <p class="description">Description</p>
-</section>
 
-<?php require_once './components/footer.php' ?>
+<?php foreach ($rows as $bookNumber => $row) :
+    [
+        "book_id" => $id,
+        "title" => $title,
+        "description" => $desciption,
+        "author" => $author,
+        "publishing_year" => $year,
+        "genre" => $genre
+    ] = $row;
+
+    echo <<<HTML
+        <section class="book">
+            <a class="bookmark fa fa-star-o" href="set_favorite.php?id={$id}" target="_blank"></a>
+            <h3>$title</h3>
+            <p class="publishing_info">
+                <span class="author">$author</span>,
+                <span class="year">$year</span>
+            </p>
+            <p class="description">$desciption</p>
+        </section>
+    HTML;
+
+endforeach;
+
+require_once './components/footer.php' ?>
