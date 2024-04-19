94ca41a50490db61d233ecaf638a7e667eb5f231
dk10119 <tranduclong092@gmail.com>
2024-04-19 17:04:05 +0300

    create a form with empty condition check on inputs

--- php/register.php
+++ php/register.php
@@ -1,5 +1,72 @@
-<?php require_once './components/header.php'; ?>
+<?php require_once './components/header.php';
 
-<p>Register</p>
+if (isset($_POST["register_form"])) {
+    try {
+        [
+            "firstname" => $fn,
+            "lastname" => $ln,
+            "email" => $email,
+            "password" => $pwd,
+            "password_retype" => $pwd_retype
+        ] = $_POST;
+
+        if ($fn == "") {
+            throw new Exception("First name cannot be empty");
+        }
+
+        if ($ln == "") {
+            throw new Exception("Last name cannot be empty");
+        }
+
+        if ($email == "") {
+            throw new Exception("Email cannot be empty");
+        }
+
+        if ($pwd == "") {
+            throw new Exception("Password cannot be empty");
+        }
+    } catch (Exception $e) {
+        $error_msg = $e->getMessage();
+    }
+}
+
+?>
+
+<form action="" method="post" class="form_container">
+    <h2>Create new user account</h2>
+    <table>
+        <tr>
+            <td><Label for="firstname">First name</Label></td>
+            <td>
+                <input type="text" name="firstname" id="firstname" value="<?= $fn ?? "" ?>">
+            </td>
+        </tr>
+        <tr>
+            <td><Label for="lastname">Last name</Label></td>
+            <td><input type="text" name="lastname" id="lastname" value="<?= $ln ?? "" ?>"></td>
+        </tr>
+        <tr>
+            <td><Label for="email">E-mail address</Label></td>
+            <td><input type="text" name="email" id="email" value="<?= $email ?? "" ?>"></td>
+        </tr>
+        <tr>
+            <td><Label for="password">Password</Label></td>
+            <td><input type="text" name="password" id="password"></td>
+        </tr>
+        <tr>
+            <td><Label for="password_retype">Retype password</Label></td>
+            <td><input type="text" name="password_retype" id="password_retype"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="submit" name="register_form" value="Register"></td>
+        </tr>
+    </table>
+
+    <?php if (isset($error_msg)) : ?>
+        <p class="error_msg"><?= $error_msg ?></p>
+    <?php endif; ?>
+
+</form>
 
 <?php require_once './components/footer.php'; ?>
