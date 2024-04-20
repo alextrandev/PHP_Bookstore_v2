5c25dd6f7cbbd3fce90ca467c1fbca02520f391e
dk10119 <tranduclong092@gmail.com>
2024-04-20 08:49:41 +0300

    fetch books and display in manage book page

--- php/manage_book.php
+++ php/manage_book.php
@@ -6,18 +6,43 @@ if (!isset($_SESSION["user"])) {
     header("Location: " . BASE_URL . "login.php?login=required");
     exit();
 }
+
+$stmt = $pdo->prepare("SELECT * FROM books");
+$stmt->execute();
+$books = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$filterGenre = $_GET["genre"] ?? true; //true mean all genre
+$filteredBooks = array_reverse(array_filter($books, fn ($book) => $book["genre"] == $filterGenre));
 ?>
 
-<h2><?= $_GET["genre"] ?? "All books" ?></h2>
-<section class="book">
-    <a href="delete_book.php?id=0"><button class="delete_button">Delete</button></a>
-    <a href="add_book.php?id=0"><button class="edit_button">Edit</button></a>
-    <h3>Book title</h3>
-    <p class="publishing_info">
-        <span class="author">Author</span>,
-        <span class="year">Year</span>
-    </p>
-    <p class="description">Description</p>
-</section>
+<h2><?= $filterGenre === true ? "All books" : $filterGenre ?></h2>
 
-<?php require_once './components/footer.php';
+<?php foreach ($filteredBooks as $filteredBook) :
+    [
+        "book_id" => $id,
+        "title" => $title,
+        "description" => $desciption,
+        "author" => $author,
+        "publishing_year" => $year,
+        "genre" => $genre
+    ] = $filteredBook;
+
+    echo <<<HTML
+        <section class="book">
+            <a href="edit_book.php?id={$id}">
+                <button class="edit_button">Edit</button>
+            </a>
+            <a href="delete_book.php?id={$id}">
+                <button class="delete_button">Delete</button>
+            </a>
+            <h3>$title</h3>
+            <p class="publishing_info">
+                <span class="author">$author</span>,
+                <span class="year">$year</span>
+            </p>
+            <p class="description">$desciption</p>
+        </section>
+    HTML;
+
+endforeach;
+
+require_once './components/footer.php';
