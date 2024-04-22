d5f02b1b575d78ecf0861060191cada9253510fd
dk10119 <tranduclong092@gmail.com>
2024-04-23 00:13:25 +0300

    create delete form and display deleting book

--- php/delete_book.php
+++ php/delete_book.php
@@ -4,8 +4,38 @@ if (!isset($_SESSION["user"])) {
     header("Location: " . BASE_URL . "login.php?login=required");
     exit();
 }
+
+if (!isset($_GET["id"])) {
+    header("Location: " . BASE_URL . "manage_book.php");
+    exit();
+}
+
+$id = $_GET["id"];
+$stmt = $pdo->prepare("SELECT * FROM books where book_id=?");
+$stmt->execute([$id]);
+$total = $stmt->rowCount();
+if (!$total) {
+    header("Location: " . BASE_URL . "manage_book.php");
+    exit();
+}
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+[
+    "title" => $title,
+    "description" => $desc,
+    "author" => $author,
+    "publishing_year" => $year
+] = $row;
 ?>
 
-<p>Delete book</p>
+<h2>Confirm delete book</h2>
+<section class="book">
+    <h3><?= $title ?></h3>
+    <p class="publishing_info">
+        <span class="author"><?= $author ?></span>,
+        <span class="year"><?= $year ?></span>
+    </p>
+    <p class="description"><?= $desc ?></p>
+</section>
+<form action="" method="post"><input type="submit" name="delete_form" value="Confirm"></form>
 
 <?php require_once './components/footer.php'; ?>
