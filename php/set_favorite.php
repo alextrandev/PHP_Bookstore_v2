1d461ff9ce22a3d661269f60c20ac07dd5c8dca2
dk10119 <tranduclong092@gmail.com>
2024-04-23 09:39:53 +0300

    create statement to add favorites book to db

--- php/set_favorite.php
+++ php/set_favorite.php
@@ -4,8 +4,26 @@ if (!isset($_SESSION["user"])) {
     header("Location: " . BASE_URL . "login.php?login=required");
     exit();
 }
-?>
 
-<p>Set favorite</p>
+if (!isset($_GET["id"])) {
+    header("Location: " . BASE_URL);
+    exit();
+}
 
-<?php require_once './components/footer.php'; ?>
+$book_id = $_GET["id"];
+$user_id = $_SESSION["user"]["user_id"];
+
+$stmt = $pdo->prepare("SELECT * FROM books WHERE book_id=?");
+$stmt->execute([$book_id]);
+$total = $stmt->rowCount();
+if (!$total) {
+    header("Location: " . BASE_URL);
+    exit();
+}
+
+$stmt = $pdo->prepare("INSERT INTO favorites (user_id, book_id) VALUES (?, ?)");
+$stmt->execute([$user_id, $book_id]);
+header("Location: " . BASE_URL);
+exit();
+
+require_once './components/footer.php';
