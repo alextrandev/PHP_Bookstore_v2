7a9d6faa7b3cfa5a4388b923a170385c0fa6aeda
dk10119 <tranduclong092@gmail.com>
2024-04-24 09:26:05 +0300

    ad pagination for index page

--- php/index.php
+++ php/index.php
@@ -4,12 +4,43 @@ $stmt = $pdo->prepare("SELECT * FROM books");
 $stmt->execute();
 $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $filterGenre = $_GET["genre"] ?? true; //true mean all genre
-$filteredBooks = array_filter($books, fn ($book) => $book["genre"] == $filterGenre);
+$filteredBooks = array_reverse(array_filter($books, fn ($book) => $book["genre"] == $filterGenre));
+
+//pagination
+$booksPerPage = 5;
+$numberofBooks = count($filteredBooks);
+$numberOfPages = ceil($numberofBooks / $booksPerPage);
+
+if (!isset($_GET["page"]) || $_GET["page"] <= 0) {
+    $currentPage = 1;
+} elseif ($_GET["page"] > $numberOfPages) {
+    $currentPage = $numberOfPages;
+} else {
+    $currentPage = intval($_GET["page"]);
+}
+
+$firstBookIndex = (intval($currentPage) - 1) * $booksPerPage;
+$displayBooks = array_slice($filteredBooks, $firstBookIndex, $booksPerPage);
 ?>
 
-<h2><?= $filterGenre === true ? "All books" : $filterGenre ?></h2>
+<div class="books_header">
+    <h2><?= $filterGenre === true ? "All books" : $filterGenre ?></h2>
+    <div class="pagination_container">
+        <a href="<?= BASE_URL . "?page=" . ($currentPage - 1) ?>">&lt; Previous</a>
+        <div class="pagination_pages">
+            <span>Pages:</span>
+            <?php for ($i = 1; $i <= $numberOfPages; $i++) : ?>
+                <a class="<?= $i == $currentPage ? "bold" : "" ?>" href="<?= BASE_URL . "?page=" . ($i) ?>">
+                    <?= $i ?>
+                </a>
+            <?php endfor; ?>
 
-<?php foreach ($filteredBooks as $filteredBook) :
+        </div>
+        <a href="<?= BASE_URL . "?page=" . ($currentPage + 1) ?>">Next &gt;</a>
+    </div>
+</div>
+
+<?php foreach ($displayBooks as $displayBook) :
     [
         "book_id" => $id,
         "title" => $title,
@@ -17,7 +48,7 @@ $filteredBooks = array_filter($books, fn ($book) => $book["genre"] == $filterGen
         "author" => $author,
         "publishing_year" => $year,
         "genre" => $genre
-    ] = $filteredBook;
+    ] = $displayBook;
 
     $favoriteButtonStyle = in_array($id, $_SESSION['favorites']) ? 'fa-star' : 'fa-star-o';
 
